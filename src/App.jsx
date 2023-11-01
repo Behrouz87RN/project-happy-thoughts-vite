91f6b574f9f7f3e1b9f17e43b36a64ad8b78265e
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-11-01 22:11:58 +0100

    initial App & thought component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,51 @@
+
+import { useState, useEffect } from "react";
+import {Thought} from './thought.jsx'
+
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  //  Initialize state for storing the API data
+  const [thoughts, setThoughts] = useState([]);
+  const url = "https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts";
+  // Create a function to fetch 
+  const getThoughts = async () => {
+    fetch(url)
+      .then((res) => {
+        //console.log("res", res);
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log("data", data);
+       setThoughts(data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+
+  useEffect(() => {
+    getThoughts();
+  }, []); 
+
+
+  return (
+    <>
+    <div className="App">
+      <input type="text" />
+      <button onClick={getThoughts}>submit</button>
+    </div>
+
+    <div className="msg">
+      {thoughts.map((thought) => {
+        return <Thought data={thought} key={thought._id}/>
+      })}
+    </div>
+  </>
+  );
+   
 };
+
+
