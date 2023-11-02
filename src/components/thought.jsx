c3360fcc8d1ea6a9316a22ceec7ab064fe403368
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-11-02 22:38:37 +0100

    update format

--- src/components/thought.jsx
+++ src/components/thought.jsx
@@ -1,37 +1,31 @@
 import { useState } from "react";
-import { LikeButton } from "./LikesButton"
+import { LikeButton } from "./LikesButton";
 
-export const Thought = ({data}) => {
-   const [hearts, setHearts] = useState(data.hearts);
-   // Like current posted thoughts (Vanessa)
-   const likeUrl = `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${data._id}/like`;
+export const Thought = ({ data }) => {
+  const [hearts, setHearts] = useState(data.hearts);
+  // Like current posted thoughts (Vanessa)
+  const likeUrl = `https://happy-thoughts-ux7hkzgmwa-uc.a.run.app/thoughts/${data._id}/like`;
 
-   const incrementLike = async () => {
-      const response = await fetch (likeUrl, {method: 'POST'});
-      const responseJson = await response.json();
+  const incrementLike = async () => {
+    const response = await fetch(likeUrl, { method: "POST" });
+    const responseJson = await response.json();
 
-      setHearts(responseJson.hearts);
-   };
+    setHearts(responseJson.hearts);
+  };
 
-   return  (
-      <div className="strMsg">
-         <p>{data.message} </p>
-         <div>
-            <div>{data.createdAt}</div>
-         <div>
-            <div>{hearts}</div>
-            <LikeButton onLike={incrementLike}/>
-         </div>
-         </div>
+  return (
+    <div className="strMsg">
+      <p>{data.message} </p>
+      <div>
+        <div>
+          <div>{hearts}</div>
+          <LikeButton onLike={incrementLike} />
+        </div>
+        <div>{data.createdAt}</div>
       </div>
-   )
-}
-  
-
-
-
-
-
+    </div>
+  );
+};
 
 // {
 //     "_id": "654298823821f40010dda668",
