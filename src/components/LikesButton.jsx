6cd9be086c303fd65535f421ca7045b4c8de307e
Vanessa Arruda <adm.vanessasouza@gmail.com>
2023-11-03 13:34:45 +0100

    add like button logic to display different heart color if clicked or not

--- src/components/LikesButton.jsx
+++ src/components/LikesButton.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 
 export const LikeButton = ({onLike}) => {
+
+
+  const [isHeartClicked, setHeartClicked] = useState (false);
+
+  const clickHeart = () => {
+    setHeartClicked(true)
+    onLike()
+  }
+
+
+  // choose which heart to display if clicked or not
   
     return (
-      <button className="likeButton" onClick={ () => onLike()} >
-         <span className="heart-icon">❤️</span>
+      <button className="likeButton" onClick={clickHeart} >
+        {isHeartClicked ? (
+          <span className="heart-icon">❤️</span>
+        ) : (
+          <span className="heart-icon">🤍</span>
+        )}
       </button>        
     )
   };
